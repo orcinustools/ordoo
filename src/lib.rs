f4a7eb42624f2c560705752e72d7b186d4c841ff
imamdigmi <imam.digmi@gmail.com>
2017-03-11 22:49:13 +0700

    REMOVE: bin dist, tests

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,59 @@
 
 extern crate hyper;
 extern crate hyperlocal;
-extern crate pencil;
 
 pub mod sock;
+
+#[test]
+fn ping() {
+    let docker = sock::Docker::new();
+    docker.ping();
+}
+
+#[test]
+fn info() {
+    let docker = sock::Docker::new();
+    docker.info();
+}
+
+#[test]
+fn images() {
+    let docker = sock::Docker::new();
+    docker.images();
+}
+
+#[test]
+fn nodes() {
+    let docker = sock::Docker::new();
+    docker.nodes();
+}
+
+#[test]
+fn tasks() {
+    let docker = sock::Docker::new();
+    docker.tasks();
+}
+
+#[test]
+fn services() {
+    let docker = sock::Docker::new();
+    docker.services();
+}
+
+#[test]
+fn volumes() {
+    let docker = sock::Docker::new();
+    docker.volumes();
+}
+
+#[test]
+fn networks() {
+    let docker = sock::Docker::new();
+    docker.networks();
+}
+
+#[test]
+fn swarm() {
+    let docker = sock::Docker::new();
+    docker.swarm();
+}
